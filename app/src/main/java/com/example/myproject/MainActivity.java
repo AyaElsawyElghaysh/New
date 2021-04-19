f941f7c51d2c63b294b702d5fe6991c332a7f4d0
Azkar <https://github.com/nermien990/Azkar>
2021-04-20 01:50:48 +0200

    add buttom navigation

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.myproject.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // enabed view Binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // bottom navigation
+        binding.bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // setting home fragment as main fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+    }
+
+    // listener nav bar
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_char:
+                            selectedFragment = new CharacterFragment();
+                            break;
+                        case R.id.nav_attr:
+                            selectedFragment = new AttributeFragment();
+                            break;
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_lib:
+                            selectedFragment = new LibraryFragment();
+                            break;
+                        case R.id.nav_aud:
+                            selectedFragment = new AudioFragment();
+                            break;
+                    }
+
+                    // begin transaction
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, selectedFragment)
+                            .commit();
+
+                    return true;
+                }
+            };
+
+}
